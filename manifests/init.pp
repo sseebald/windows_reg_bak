1010766809f54527b23ba6865957bf9985c1a268
sseebald <sseebald@gmail.com>
2014-02-19 19:44:29 +0000

    Fixed powershell logic in code to reference correct fact, added double \\

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,7 @@
 class windows_reg_bak {
 
   exec {'Backup registry':
-    command  => "Reg export HKLM C:\Users\vagrant\Documents\${::hostname}.reg",
-    #creates  => "C:/${::hostname}.reg",
+    command  => "Reg export HKLM C:\\Users\\vagrant\\Documents\\${::hostname}.reg",
     provider => powershell,
   }
 
